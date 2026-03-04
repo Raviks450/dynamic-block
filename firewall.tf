52d898b21c91585901a7bef4715caf9deae3ccfb
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-03-04 13:13:57 +0000

    Create firewall.tf

---
+++ firewall.tf
@@ -0,0 +1,33 @@
+variable "firewall_rules" {
+  type = map(object({
+    protocol = string
+    ports    = list(string)
+  }))
+  default = {
+    "web" = {
+      protocol = "tcp"
+      ports    = ["80", "443"]
+    }
+    "admin" = {
+      protocol = "tcp"
+      ports    = ["22"]
+    }
+  }
+}
+
+resource "google_compute_firewall" "dynamic_ingress" {
+  name    = "example-dynamic-firewall"
+  network = "default"
+
+  # The "allow" label matches the nested block name in GCP
+  dynamic "allow" {
+    for_each = var.firewall_rules
+    
+    content {
+      protocol = allow.value.protocol
+      ports    = allow.value.ports
+    }
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
